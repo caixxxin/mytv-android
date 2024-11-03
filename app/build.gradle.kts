7a831d0be5e8661a4f9b74126b5f1a7d66e9685d
caixxxin <gamelook2003@gmail.com>
2024-11-03 23:35:41 +0800

    更新参数。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,22 +60,22 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    signingConfigs {
-        create("release") {
-            storeFile =
-                file(System.getenv("KEYSTORE") ?: keystoreProperties["storeFile"] ?: "keystore.jks")
-            storePassword = System.getenv("KEYSTORE_PASSWORD")
-                ?: keystoreProperties.getProperty("storePassword")
-            keyAlias = System.getenv("KEY_ALIAS") ?: keystoreProperties.getProperty("keyAlias")
-            keyPassword =
-                System.getenv("KEY_PASSWORD") ?: keystoreProperties.getProperty("keyPassword")
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
+    // signingConfigs {
+    //     create("release") {
+    //         storeFile =
+    //             file(System.getenv("KEYSTORE") ?: keystoreProperties["storeFile"] ?: "keystore.jks")
+    //         storePassword = System.getenv("KEYSTORE_PASSWORD")
+    //             ?: keystoreProperties.getProperty("storePassword")
+    //         keyAlias = System.getenv("KEY_ALIAS") ?: keystoreProperties.getProperty("keyAlias")
+    //         keyPassword =
+    //             System.getenv("KEY_PASSWORD") ?: keystoreProperties.getProperty("keyPassword")
+    //     }
+    // }
+    // buildTypes {
+    //     getByName("release") {
+    //         signingConfig = signingConfigs.getByName("release")
+    //     }
+    // }
 }
 
 dependencies {
